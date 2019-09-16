99519edcc8433654c0893c47e9da5977e2416709
Arpan Patel <adp8ke@gmail.com>
2019-09-16 16:25:14 -0400

    making stubbs for service

---
+++ src/rides/rides-service.js
@@ -0,0 +1,23 @@
+const RidesService = {
+
+//query db based on search params
+  getSearchedRides(){},
+
+  //insert ride from driver form
+  addNewDriverRide(){},
+
+  //remove entire ride
+  deleteDriverRide(){},
+
+  //get all rides per passenger id
+  getAllPassengerRides(){},
+
+  //insert passenger to ride id
+  addPassengerToRide(){},
+
+  //delete passenger from ride
+  removePassengerFromRide(){},
+
+};
+
+module.exports = RidesService;
