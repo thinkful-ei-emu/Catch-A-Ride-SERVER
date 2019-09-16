b0f34f7fa9f55f95467d00763040e646a7c1b81a
Arpan Patel <adp8ke@gmail.com>
2019-09-16 15:22:48 -0400

    initial routes working on postman

--- src/rides/rides-router.js
+++ src/rides/rides-router.js
@@ -10,7 +10,7 @@ ridesRouter
   //get rides available using service from db
   .get((req, res, next) => {
     //take req.body and descturcture, query db to get search results based on body params
-    res.status(200).json();
+    res.status(200).json('get /');
   });
 
 ridesRouter
@@ -19,13 +19,14 @@ ridesRouter
   //post driver form and add to rides db
   .post(jsonBodyParser, (req, res, next) => {
     //take req.body and descructure, add into db
-    res.status(201);
+    res.status(201).json('post /driver');
   })
 
   //take delete request and delete driver's ride from db 
   .delete((req, res, next) => {
 
     res.status(204);
+    //got 204 no content when testing on postman
   });
 
 ridesRouter
@@ -33,17 +34,20 @@ ridesRouter
 
 //get passenger specific rides
   .get((req, res, next) => {
-    res.status(200).json();
+    res.status(200).json('get /passenger');
   })
 
 //passenger clicks reserve/add to ride and update db p1, p2, whichever next is null
   .post(jsonBodyParser, (req, res, next) => {
-    res.status(201);
+    res.status(201).json('post /passenger');
   })
 
 //passenger clicks delete and update db p1, p2, whichever matches passanger
   .delete((req, res, next) => {
     res.status(204);
+    //got 204 no content when testing on postman
   });
 
     
+
+module.exports = ridesRouter;
