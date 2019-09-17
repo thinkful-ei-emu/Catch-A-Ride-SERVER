e9b8e29a084304920437d9befdab695d08448946
Arpan Patel <adp8ke@gmail.com>
2019-09-17 10:46:12 -0400

    did initial mockup of what the queries may look like

--- src/rides/rides-service.js
+++ src/rides/rides-service.js
@@ -1,25 +1,100 @@
 const RidesService = {
 
-//query db based on search params
-  getSearchedRides(){},
+  //query db based on search params
+  getSearchedRides(){
+    //takes db, starting loc, ending loc
+
+    // return db
+    //   .from('rides')
+    //   .select('*')
+    //   .where({
+    //     starting_loc: startingLoc,
+    //     ending_loc: endingLoc,
+    //   });
+
+    // knex('users').where({
+    //   first_name: 'Test',
+    //   last_name:  'User'
+    // }).select('id')
+    // Outputs: select `id` from `users` where `first_name` = 'Test' and `last_name` = 'User'
+  },
+
 
   //insert ride from driver form
-  addNewDriverRide(){},
+  addNewDriverRide(){
+    //takes db, newRide
+
+  //   return db
+  //     .insert(newRide)
+  //     .into('rides')
+  //     .returning('*')
+  //     .then(([ride]) => ride)
+  // },
 
   //remove entire ride
-  deleteDriverRide(){},
+  deleteDriverRide(){
+    //takes db, ride_Id
+    
+    // return db
+    //   .where('id', ride_id)
+    //   .del();
+  },
 
   //get all rides per passenger id
-  getAllPassengerRides(){},
+  getAllPassengerRides(){
+    //takes db, pass_id
+
+    // return db
+    //   .from('rides')
+    //   .select('*')
+    //   .where('p1', pass_id)
+    //   .orWhere({
+    //     p2: pass_id,
+    //   })
+    //   .orWhere({
+    //     p3: pass_id,
+    //   })
+    //   .orWhere({
+    //     p4: pass_id,
+    //   })
+    //   .orWhere({
+    //     p5: pass_id,
+    //   })
+    //   .orWhere({
+    //     p6: pass_id,
+    //   });
+  },
 
   //insert passenger to ride id
-  addPassengerToRide(){},
+  addPassengerToRide(){
+    //takes db, pass_id, ride_id
+
+    // return db
+    //   .where('id', ride_id)
+    //   .update({
+    //     //which column is not null and set column value to pass_id
+    //   })
+  },
 
   //delete passenger from ride
-  removePassengerFromRide(){},
+  removePassengerFromRide(){
+    //takes db, pass_id, and ride_id
+
+    // return db
+    //   .where('id', ride_id)
+    //   .update({
+    //     //which column containes pass_id, and then update to null
+    //   })
+  },
 
   //get single ride by id prolly
-  getSingleRide(){}
+  getSingleRide(){
+    //takes db and ride_id
+    // return db
+    //   .from('rides')
+    //   .where('id', ride_id)
+    //   .first();
+  }
 
 };
 
