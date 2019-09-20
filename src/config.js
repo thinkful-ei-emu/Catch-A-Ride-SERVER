871eb2b44eca5f46efb9bdd4400179690b5356c8
Andrew <andrewyin1994@gmail.com>
2019-09-20 14:25:48 -0700

    updated heroku config files

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   CLIENT_ID: process.env.CLIENT_ID || 'Google API Client ID here',
   CLIENT_SECRET: process.env.CLIENT_SECRET || 'Google API Client Secret here',
-  DB_URL: process.env.DB_URL,
-  TEST_ID_TOKEN: process.env.TEST_ID_TOKEN
+  DB_URL: process.env.DATABASE_URL || process.env.DB_URL || 'postgresql://scooter@localhost/catch-a-ride',
+  TEST_ID_TOKEN: process.env.TEST_ID_TOKEN || 'Google OAuth Test Token Here'
 };
