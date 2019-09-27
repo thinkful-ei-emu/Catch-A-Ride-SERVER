e062860603b9712309d815ed5496b9f114a392d2
Arpan Patel <adp8ke@gmail.com>
2019-09-27 10:57:38 -0400

    tryna fix timezone stuff for front end might

--- src/migrations/004.undo.create_date_time_column.sql
+++ src/migrations/004.undo.create_date_time_column.sql
@@ -1,5 +1,5 @@
-ALTER TABLE rides DROP COLUMN date_time
+ALTER TABLE rides DROP COLUMN date_time;
 
-ALTER TABLE rides ADD date date not null
+ALTER TABLE rides ADD date date;
 
-ALTER TABLE rides ADD time time not null;
+ALTER TABLE rides ADD time time;
