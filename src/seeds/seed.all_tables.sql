e342c4ebbda2d539e47e6baf240fdacc617d535c
Arpan Patel <adp8ke@gmail.com>
2019-10-01 10:17:00 -0400

    changed seed file so andrew doesnt get anymore emails

--- src/seeds/seed.all_tables.sql
+++ src/seeds/seed.all_tables.sql
@@ -16,8 +16,8 @@ BEGIN;
 
   INSERT INTO rides ("driver_id", "starting", "destination", "date_time", "description", "capacity", "p1", "p2", "p3", "p4", "p5", "p6", "p7", "driver_name")
   VALUES
-  (113938795112280626788, 'Blacksburg', 'Virginia Beach', '2019-09-16 08:00:00', '$10 for gas', 4, 112539454699313514648, null, null, null, null, null, null, 'Andrew Yin'),
-  (113938795112280626788, 'Virginia Beach', 'Blacksburg', '2019-09-17 14:00:00', '$10 for gas, limited trunk space', 4, 101820519124146512532, 112539454699313514648, null, null, null, null, null, 'Andrew Yin'),
-  (103339967984381402998, 'Blacksburg', 'Fairfax', '2019-09-17 18:00:00', 'No charge, hop in', 5, 113938795112280626788, 101820519124146512532, 112539454699313514648, null, null, null, null, 'Bob Smith');
+  (101820519124146512532, 'Blacksburg', 'Virginia Beach', '2019-09-16 08:00:00', '$10 for gas', 4, 112539454699313514648, null, null, null, null, null, null, 'Andrew Yin'),
+  (103339967984381402998, 'Virginia Beach', 'Blacksburg', '2019-09-17 14:00:00', '$10 for gas, limited trunk space', 4, 101820519124146512532, 112539454699313514648, null, null, null, null, null, 'Andrew Yin'),
+  (103339967984381402998, 'Blacksburg', 'Fairfax', '2019-09-17 18:00:00', 'No charge, hop in', 5, 112539454699313514648, 101820519124146512532, null, null, null, null, null, 'Bob Smith');
 
 COMMIT;
