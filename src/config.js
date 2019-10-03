cd4c90c6dd857d3464728306d6573c7661fa00d0
Andrew <andrewyin1994@gmail.com>
2019-10-03 14:11:52 -0700

    fixing merge conflicts

--- src/config.js
+++ src/config.js
@@ -4,12 +4,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   CLIENT_ID: process.env.CLIENT_ID || 'Google API Client ID here',
   CLIENT_SECRET: process.env.CLIENT_SECRET || 'Google API Client Secret here',
-<<<<<<< HEAD
   DB_URL: process.env.DATABASE_URL || process.env.DB_URL || 'postgresql://scooter@localhost/catch-a-ride',
-  TEST_ID_TOKEN: process.env.TEST_ID_TOKEN || 'Google OAuth Test Token Here'
-=======
-  DB_URL: process.env.DB_URL,
-  TEST_ID_TOKEN: process.env.TEST_ID_TOKEN,
+  TEST_ID_TOKEN: process.env.TEST_ID_TOKEN || 'Google OAuth Test Token Here',
   GEO_API_KEY: process.env.GEO_API_KEY
->>>>>>> cedb5e2a56c74f72bfedb7ba1a49bedaf7c790d1
 };
